7466aa08b3a548daaf9d7e88af9ec85287c4b2f6
Vu <longvunguyen2003@gmail.com>
2023-08-23 15:53:28 +0700

    MODIFY script.js Add a function that plays a single round

--- script.js
+++ script.js
@@ -3,4 +3,20 @@ function getComputerChoice(){
     //return computer's choice
     return choices[Math.floor(Math.random() * 3)];
 }
-console.log(getComputerChoice());
+//functoin plays a single round of Rock Paper Scissors
+function playSingleRound(playerSelection, computerSelection){
+    //ignore case of selections by capitalizing
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase();
+
+    //determine winner and loser
+    if (playerSelection === computerSelection){
+        return "Draw!"
+    }
+    else if ((playerSelection == "Rock" && computerSelection == "Paper") || (playerSelection == "Scissors" && computerSelection == "Rock") || (playerSelection == "Paper" && computerSelection == "Scissors")){
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+    else{
+        return `You won! ${playerSelection} beats ${computerSelection}.`;
+    }
+}
