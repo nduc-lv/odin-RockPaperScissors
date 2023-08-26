3505de181eecd95b93c525add67fb426ca3a807c
Vu <longvunguyen2003@gmail.com>
2023-08-26 11:52:46 +0700

    PLAY FIVE ROUNDS Display the running score, and announce a winner of the game after 5 rounds.

--- script.js
+++ script.js
@@ -58,21 +58,54 @@ function game(){
 }
 game();
 */
-
-//create three button, each for each selection
 const parent = document.querySelector(`body`);
+//create three button, each for each selection
 const rock = document.createElement(`button`);
 const scissors = document.createElement(`button`);
 const paper = document.createElement(`button`);
 const button = [rock, paper, scissors];
 //create a div for display results
 const div = document.createElement(`div`);
+
+//keep track of score and number of rounds
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
 //make the button react when be clicked on
 for (let i = 0; i < 3; i++){
+    //add text to button
+    button[i].innerText = choices[i];
     let computerChoice = getComputerChoice();
     button[i].addEventListener(`click`, () => {
+        round++;
         //display the result
-        div.textContent = playSingleRound(choices[i], computerChoice);
+        let message = playSingleRound(choices[i], computerChoice);
+        if (message[4] == `l`){//Lose
+            computerScore++;
+        }
+        else if (message[4] == `w`){
+            playerScore++;
+        }
+        let scoreBoard = ` Player: ${playerScore}  Computer: ${computerScore}` + `<br>`;
+        div.innerHTML += message + scoreBoard;
+
+        //determine the winner after 5 round
+        if (round == 5){
+            if (computerScore == playerScore){
+                div.innerHTML += "Tie<br>";
+            }
+            else{
+                let winner = "Computer";
+                if (playerScore > computerScore){
+                    winner = "Player"
+                }
+                div.innerHTML += `${winner} WIN!<br>` + scoreBoard;
+            }
+            round = 0;
+            computerScore = 0;
+            playerScore = 0;
+            div.innerHTML += `<br> GAME OVER! <br> RESTART <br>`;
+        }
     });
     
 }
